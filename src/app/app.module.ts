01ac7ef03f213d9c7033e1a4c3a7acd9116e0877
loganprowley <matthewvickers216@gmail.com>
2018-08-17 15:28:34 -0700

    Add bottom navbar component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { BottomBarComponent } from './bottom-bar/bottom-bar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    BottomBarComponent
   ],
   imports: [
     BrowserModule
