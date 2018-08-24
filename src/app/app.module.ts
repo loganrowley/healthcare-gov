8cfb5f1103d4eea203abee3e379383e6cb2d08fb
loganprowley <matthewvickers216@gmail.com>
2018-08-24 15:34:14 -0700

    Working on img-transition component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { masterFirebaseConfig } from './api-keys';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
@@ -30,7 +32,7 @@ export const firebaseConfig = {
     BrowserModule
     FormsModule,
     HttpModule,
-    routing
+    routing,
   ],
   providers: [],
   bootstrap: [AppComponent]
